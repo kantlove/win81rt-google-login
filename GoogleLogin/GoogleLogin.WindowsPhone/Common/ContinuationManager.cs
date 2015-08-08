a86f6883393178fb5d7e73de1c5e45028ab261d2
minhthai <minhthai40@gmail.com>
2015-08-08 14:12:46 +0700

    first initialization

---
+++ GoogleLogin/GoogleLogin.WindowsPhone/Common/ContinuationManager.cs
@@ -0,0 +1,42 @@
+﻿using Windows.ApplicationModel.Activation;
+using GoogleLogin;
+
+namespace GoogleLogin.Common
+{
+    /// <summary>
+    /// ContinuationManager is used to detect if the most recent activation was due to a
+    /// authentication continuation.
+    ///
+    /// Note: To keep this sample as simple as possible, the content of the file was changed to support
+    /// WebAuthenticationBrokerContinuation ONLY.
+    /// Take a look in http://msdn.microsoft.com/en-us/library/dn631755.aspx
+    /// for a full documentation on how to support continuation in other cases.
+    /// </summary>
+    public class ContinuationManager
+    {
+        /// <summary>
+        /// Sets the ContinuationArgs for this instance.
+        /// Should be called by the main activation handling code in App.xaml.cs.
+        /// </summary>
+        /// <param name="args">The activation args.</param>
+        internal void Continue(IContinuationActivatedEventArgs args)
+        {
+            if (args.Kind == ActivationKind.WebAuthenticationBrokerContinuation)
+            {
+                var page = MainPage.Current as IWebAuthenticationContinuable;
+                page?.ContinueWebAuthentication(
+                    args as WebAuthenticationBrokerContinuationEventArgs);
+            }
+        }
+    }
+
+    /// <summary>Implement this interface if your page invokes the web authentication broker.</summary>
+    interface IWebAuthenticationContinuable
+    {
+        /// <summary>
+        /// This method is invoked when the web authentication broker returns with the authentication result.
+        /// </summary>
+        /// <param name="args">Activated event args object that contains returned authentication token.</param>
+        void ContinueWebAuthentication(WebAuthenticationBrokerContinuationEventArgs args);
+    }
+}
---
+++ GoogleLogin/GoogleLogin.WindowsPhone/MainPage.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using GoogleLogin.Common;
+using Google.Apis.Oauth2.v2;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace GoogleLogin
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, IWebAuthenticationContinuable
+    {
+        public static MainPage Current { get; private set; }
+        private UserCredential _credential;
+        private Oauth2Service _service;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+            Current = this;
+        }
+
+        public async void ContinueWebAuthentication(
+    WebAuthenticationBrokerContinuationEventArgs args)
+        {
+            await PasswordVaultDataStore.Default.StoreAsync<SerializableWebAuthResult>(
+                SerializableWebAuthResult.Name,
+                new SerializableWebAuthResult(args.WebAuthenticationResult));
+
+            await GetInfo();
+
+            await PasswordVaultDataStore.Default.DeleteAsync<SerializableWebAuthResult>(
+                SerializableWebAuthResult.Name);
+        }
+
+        private async Task AuthenticateAsync()
+        {
+            if (_service != null)
+                return;
+
+            _credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                new Uri("ms-appx:///Assets/client_secrets.json"),
+                new[] { Oauth2Service.Scope.UserinfoEmail },
+                "user",
+                CancellationToken.None);
+
+            var initializer = new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = _credential,
+                ApplicationName = "GoogleLogin",
+            };
+
+            _service = new Oauth2Service(initializer);
+
+        }
+
+        private void GetFiles_Click(object sender, RoutedEventArgs e)
+        {
+            DoSomething();
+            
+        }
+
+        async void DoSomething()
+        {
+            await AuthenticateAsync();
+            
+        }
+
+        async Task GetInfo()
+        {
+            await AuthenticateAsync();
+
+            var userinfo = await _service.Userinfo.Get().ExecuteAsync();
+            tbMail.Text = userinfo.Email;
+        }
+
+    }
+}
